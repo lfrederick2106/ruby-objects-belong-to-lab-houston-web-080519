a3fba399f13bb61f9d2cd13c1911e4bb7b8f94db
inherited-chain-1307 <lfrederick8@hotmail.com>
2019-08-12 01:56:12 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -3,15 +3,8 @@ class Author
  
   def initialize
     @name = name
-    #@genre = genre
   end
   
-  # def name
-  #   @name
-  # end
- 
 end
 
 jhumpa_lahiri = Author.new
-#("Beyonce", "Pop")
-#puts artist.name
