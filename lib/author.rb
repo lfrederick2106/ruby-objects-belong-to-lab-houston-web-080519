1b9ee1b65ca87029d0fc02563b8469f786762c67
inherited-chain-1307 <lfrederick8@hotmail.com>
2019-08-12 01:49:27 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,17 @@
+class Author
+  attr_accessor :name, :genre
+ 
+  def initialize
+    @name = name
+    @genre = genre
+  end
+  
+  # def name
+  #   @name
+  # end
+ 
+end
+
+beyonce = Artist.new
+#("Beyonce", "Pop")
+#puts artist.name
